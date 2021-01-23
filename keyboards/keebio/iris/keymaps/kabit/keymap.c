57d0a823ed627dc6c2d9a98c7e039f741c5d939b
Tarah Tamayo <tarah@tarah.us>
2021-01-23 03:23:34 +0000

    needed semicolon

--- keyboards/keebio/iris/keymaps/kabit/keymap.c
+++ keyboards/keebio/iris/keymaps/kabit/keymap.c
@@ -215,7 +215,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                      _______, _______, _______,                   _______, _______, _______
   //                               └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   )
-}
+};
 
 void keyboard_post_init_user(void) {
     // Enable the LED layers
