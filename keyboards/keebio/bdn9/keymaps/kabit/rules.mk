e0021d212f6b04f4d1dd2ffcf4e696edac711d74
Tarah Z. Tamayo <Tarah@Tarah.us>
2021-04-19 20:15:35 -0400

    Create rules.mk
    from iris/kabit

---
+++ keyboards/keebio/bdn9/keymaps/kabit/rules.mk
@@ -0,0 +1,9 @@
+# Memory reduction
+LTO_ENABLE = yes
+MOUSEKEY_ENABLE = no
+CONSOLE_ENABLE = no
+
+# Features used
+RGBLIGHT_ENABLE = yes       # Backlighting
+ENCODER_ENABLE = yes        # Linear Encoder functionality
+#CONSOLE_ENABLE = yes        # DEBUG
