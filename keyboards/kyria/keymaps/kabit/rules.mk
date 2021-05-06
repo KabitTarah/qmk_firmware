7af40ecc8c2803e5db5fb1da55a4aad2c37f4d02
Tarah Z. Tamayo <Tarah@Tarah.us>
2021-05-05 21:48:59 -0400

    Update rules.mk

--- keyboards/kyria/keymaps/kabit/rules.mk
+++ keyboards/kyria/keymaps/kabit/rules.mk
@@ -1,4 +1,11 @@
+# Memory reduction
+LTO_ENABLE = yes
+MOUSEKEY_ENABLE = no
+CONSOLE_ENABLE = no
+
 OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
 LEADER_ENABLE = yes        # Enable Leader key sequences
+
+CONSOLE_ENABLE = yes        # DEBUG
